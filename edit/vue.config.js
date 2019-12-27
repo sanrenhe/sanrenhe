dc387134940c155a99b98f1767267257b8f63bda
秦春臻 <qinchunzhen@urthink.com>
2019-12-27 19:01:39 +0800

    refactor

--- edit/vue.config.js
+++ edit/vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
-    publicPath: "/",
+    publicPath: "./",
     outputDir: __dirname.replace('edit', 'docs'),// "dist"
     productionSourceMap: false,
     filenameHashing: false,
